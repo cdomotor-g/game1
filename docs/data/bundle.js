04e1c20d19d567a13bff1ad0a6c631cd6447ffea
cdomotor-g <cameron.domotor@gmail.com>
2026-09-03 03:57:36 +1000

    The event-earthquake plate lands

--- docs/data/bundle.js
+++ docs/data/bundle.js
@@ -17928,6 +17928,17 @@ window.GAME_DATA = {
           1
         ]
       },
+      "event-earthquake": {
+        "file": "art/renders/event-earthquake.png",
+        "width": 1536,
+        "height": 1024,
+        "subject": [
+          0,
+          0,
+          1,
+          1
+        ]
+      },
       "event-flood": {
         "file": "art/renders/event-flood.png",
         "width": 1536,
